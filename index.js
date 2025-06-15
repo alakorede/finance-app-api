480656e1700562f69644010a9de72aaf7630c4ff
Luiz Moraes <luiz.moraes@tvfsoftware.com>
2025-06-15 00:45:12 -0300

    feat: add init miogration to create users and transactions tables

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 //procure importar o dotenv primeiro e com o seguinte formato quando estiver trabalhando com ESModule ao invés do CommonJS
+//Isso garante que ele é importado antes de tudo e que a configuração dele é feita antes de tudo,
+// garantindo que o acesso do process.env esteja disponível pra todo o dódigo
 import "dotenv/config.js"
 import express from "express"
 
