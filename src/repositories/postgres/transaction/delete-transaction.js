19ad656916465463e462fae15e5286e27e3eb533
Luiz Moraes <luiz.moraes@tvfsoftware.com>
2025-06-23 01:19:37 -0300

    fix: updated query from users to transactions table

--- src/repositories/postgres/transaction/delete-transaction.js
+++ src/repositories/postgres/transaction/delete-transaction.js
@@ -3,9 +3,10 @@ import { PostgresHelper } from "../../../db/postgres/helper.js"
 export class PostgresDeleteTransactionRepository {
     async execute(transactionId) {
         const deletedTransaction = await PostgresHelper.query(
-            "DELETE FROM users WHERE id = $1 RETURNING *",
+            "DELETE FROM transactions WHERE id = $1 RETURNING *",
             [transactionId],
         )
+
         return deletedTransaction[0]
     }
 }
