5e4c8ad4f524ba75d11b45dc71992f1f5d48905a
Luiz Moraes <luiz.moraes@tvfsoftware.com>
2025-06-23 13:29:50 -0300

    feat: add GetUserBalanceUseCase

---
+++ src/use-cases/user/get-user-balance.js
@@ -0,0 +1,15 @@
+import { UserNotFoundError } from "../../errors/user.js"
+
+export class GetUserBalanceUseCase {
+    constructor(getUserBalanceRepository, getUserByIdRepository) {
+        this.getUserBalanceRepository = getUserBalanceRepository
+        this.getUserByIdRepository = getUserByIdRepository
+    }
+    async execute(userId) {
+        const userExists = await this.getUserByIdRepository.execute(userId)
+        if (!userExists) {
+            throw new UserNotFoundError()
+        }
+        return await this.getUserBalanceRepository.execute(userId)
+    }
+}
