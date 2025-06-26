98fe089bdbe6f302748f0b4e914248dee79b7d80
Luiz Moraes <luiz.moraes@tvfsoftware.com>
2025-06-26 02:02:25 -0300

    feat: added example of aggregate to create the same effect of a view

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -6,5 +6,35 @@ export class PostgresGetUserBalanceRepository {
         return await prisma.$queryRaw`
         SELECT * FROM user_balance_view WHERE user_id = ${userId}
       `
+
+        //Se eu não tivesse a view, usaria o o agregate pra fazer essa consulta, segue exemplo:
+        // const {_sum: {amount: totalExpenses}} = await prisma.transaction.aggregate({
+        //     where: {
+        //         user_id: userId,
+        //         type: "EXPENSE",
+        //     },
+        //     _sum: {
+        //         amount: true,
+        //     },
+        // })
+        // const {_sum: {amount: totalEarnings}} = await prisma.transaction.aggregate({
+        //     where: {
+        //         user_id: userId,
+        //         type: "EARNING",
+        //     },
+        //     _sum: {
+        //         amount: true,
+        //     },
+        // })
+        // const {_sum: {amount: totalInvestments}} = await prisma.transaction.aggregate({
+        //     where: {
+        //         user_id: userId,
+        //         type: "INVESTMENT",
+        //     },
+        //     _sum: {
+        //         amount: true,
+        //     },
+        // })
+        // const balance = totalEarnings - totalExpenses - totalInvestments
     }
 }
