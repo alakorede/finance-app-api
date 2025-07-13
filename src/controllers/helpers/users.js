4cbc9e445d8e9a267462c32d10b484dc9ac3435b
Luiz Moraes <luizmoraesim@gmail.com>
2025-07-12 23:12:44 -0300

    fix: added return error messages on body.message variable"

--- src/controllers/helpers/users.js
+++ src/controllers/helpers/users.js
@@ -2,9 +2,9 @@ import validator from "validator"
 import { serverReturn } from "./http.js"
 
 export const emailAlreadyInUseResponse = () =>
-    serverReturn(400, "E-mail already in use")
+    serverReturn(400, { message: "E-mail already in use" })
 
 export const invalidIdResponse = () =>
-    serverReturn(400, "Id must be provided and must be an UUID")
+    serverReturn(400, { message: "Id must be provided and must be an UUID" })
 
 export const isIdValid = (userId) => userId && validator.isUUID(userId)
