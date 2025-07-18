51f62a89c4212b80e7ce5df9ddd9e166d5b1405f
Luiz Moraes <luizmoraesim@gmail.com>
2025-07-18 01:06:44 -0300

    test: add UpdateUserController tests

--- test/controllers/user/update-user.test.js
+++ test/controllers/user/update-user.test.js
@@ -1,15 +1,13 @@
 import { UpdateUserController } from "../../../src/controllers/user/update-user.js"
+import { EmailAlreadyInUseError } from "../../../src/errors/user.js"
 import { faker } from "@faker-js/faker"
 
 describe("UpdateUserController", () => {
     class UpdateUserUseCaseStub {
-        async execute() {
+        async execute(userId, updateParams) {
             return {
-                id: faker.string.uuid(),
-                first_name: faker.person.firstName(),
-                last_name: faker.person.lastName(),
-                email: faker.internet.email(),
-                password: faker.internet.password({ length: 7 }),
+                id: userId,
+                ...updateParams,
             }
         }
     }
@@ -66,6 +64,116 @@ describe("UpdateUserController", () => {
         expect(result.statusCode).toBe(400)
         expect(result.body.message).toBe(
             "Id must be provided and must be an UUID",
+        )
+    })
+
+    test("should return statusCode 400 and message on body related to first_name provided with fails", async () => {
+        //arrange
+        const { sut } = makeSut()
+
+        //act
+        const result = await sut.execute({
+            params: httpRequest.params,
+            body: { ...httpRequest.body, first_name: " " },
+        })
+
+        //assert
+        expect(result.statusCode).toBe(400)
+        expect(result.body.message).toBe("first_name required")
+    })
+
+    test("should return statusCode 400 and message on body related to last_name provided with fails", async () => {
+        //arrange
+        const { sut } = makeSut()
+
+        //act
+        const result = await sut.execute({
+            params: httpRequest.params,
+            body: { ...httpRequest.body, last_name: " " },
+        })
+
+        //assert
+        expect(result.statusCode).toBe(400)
+        expect(result.body.message).toBe("last_name required")
+    })
+
+    test("should return statusCode 400 and message on body related to email is provided but empty", async () => {
+        //arrange
+        const { sut } = makeSut()
+
+        //act
+        const result = await sut.execute({
+            params: httpRequest.params,
+            body: { ...httpRequest.body, email: " " },
+        })
+
+        //assert
+        expect(result.statusCode).toBe(400)
+        expect(result.body.message).toBe("Provide a valid e-mail")
+    })
+
+    test("should return statusCode 400 and message on body related to email is provided with wrong format", async () => {
+        //arrange
+        const { sut } = makeSut()
+
+        //act
+        const result = await sut.execute({
+            params: httpRequest.params,
+            body: { ...httpRequest.body, email: "invalid_email" },
+        })
+
+        //assert
+        expect(result.statusCode).toBe(400)
+        expect(result.body.message).toBe("Provide a valid e-mail")
+    })
+
+    test("should return statusCode 400 and message on body related to password with less than 6 characteres", async () => {
+        //arrange
+        const { sut } = makeSut()
+
+        //act
+        const result = await sut.execute({
+            params: httpRequest.params,
+            body: {
+                ...httpRequest.body,
+                password: faker.internet.password({ length: 3 }),
+            },
+        })
+
+        //assert
+        expect(result.statusCode).toBe(400)
+        expect(result.body.message).toBe(
+            "Password must have at least 6 characters",
+        )
+    })
+
+    test("Should return 404 on user not found error when userId does not exist on DB", async () => {
+        //arrange
+        const { updateUserUseCase, sut } = makeSut()
+
+        jest.spyOn(updateUserUseCase, "execute").mockReturnValueOnce(null)
+        //act
+        const result = await sut.execute(httpRequest)
+        //assert
+        expect(result.statusCode).toBe(404)
+        expect(result.body.message).toBe("User Not Found")
+    })
+
+    test("Should return statusCode 400and related message on body if UpdateUserUseCase throws EmailIsAlreadyInUseError  ", async () => {
+        //arrange
+        const { updateUserUseCase, sut } = makeSut()
+
+        //act
+        jest.spyOn(updateUserUseCase, "execute").mockImplementationOnce(() => {
+            throw new EmailAlreadyInUseError(httpRequest.body.email)
+        })
+
+        const result = await sut.execute(httpRequest)
+
+        //assert
+        expect(result.statusCode).toBe(400)
+        expect(result.body.message).toBe(
+            "The provided e-mail is already in use.",
         )
     })
 })
