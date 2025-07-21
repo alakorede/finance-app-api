57c60c55d2754602b912a9899b807ae4bb732910
Luiz Moraes <luizmoraesim@gmail.com>
2025-07-21 01:53:06 -0300

    fix: logic on DeleteTransactionController fixed to return TransactionNotFound on id not found

--- src/controllers/transaction/delete-transaction.js
+++ src/controllers/transaction/delete-transaction.js
@@ -1,19 +1,31 @@
 import { isIdValid, invalidIdResponse, serverReturn } from "../helpers/index.js"
+import { TransactionNotFoundError } from "../../errors/transaction.js"
 
 export class DeleteTransactionController {
     constructor(deleteTransactionUseCase) {
         this.deleteTransactionUseCase = deleteTransactionUseCase
     }
     async execute(httpRequest) {
-        const transactionId = httpRequest.params.transactionId
+        try {
+            const transactionId = httpRequest.params.transactionId
 
-        if (!isIdValid(transactionId)) {
-            return invalidIdResponse()
+            if (!isIdValid(transactionId)) {
+                return invalidIdResponse()
+            }
+
+            const deletedTransaction =
+                await this.deleteTransactionUseCase.execute(transactionId)
+
+            if (!deletedTransaction) {
+                throw new TransactionNotFoundError()
+            }
+
+            return serverReturn(200, deletedTransaction)
+        } catch (e) {
+            if (e instanceof TransactionNotFoundError) {
+                return serverReturn(404, { message: e.message })
+            }
+            return serverReturn(500, "Internal Server Error")
         }
-
-        const deletedTransaction =
-            await this.deleteTransactionUseCase.execute(transactionId)
-
-        return serverReturn(200, deletedTransaction)
     }
 }
