f4b65afa248d453dc9d61a223bc0b60d093659f4
Luiz Moraes <luizmoraesim@gmail.com>
2025-07-23 02:46:04 -0300

    test: add Internal Server Error test for UpdateTransactionController

--- test/controllers/transaction/update-transaction.test.js
+++ test/controllers/transaction/update-transaction.test.js
@@ -220,4 +220,19 @@ describe("UpdateTransactionController", () => {
             httpRequest.body,
         )
     })
+
+    test("Should return error 500 and Internal Error message on Error", async () => {
+        //arrange
+        const { updateTransactionUseCase, sut } = makeSut()
+        jest.spyOn(updateTransactionUseCase, "execute").mockImplementationOnce(
+            async () => {
+                throw new Error()
+            },
+        )
+        //act
+        const result = await sut.execute(httpRequest)
+        //assert
+        expect(result.statusCode).toBe(500)
+        expect(result.body.message).toBe("Internal Server Error")
+    })
 })
