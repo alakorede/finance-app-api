db953f0279367800b1d77fb4d535c38532598e29
Luiz Moraes <luizmoraesim@gmail.com>
2025-07-23 01:18:38 -0300

    fix: threating transaction not found fail on PostgresUpdateTransactionRepository

--- src/repositories/postgres/transaction/update-transaction.js
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -1,11 +1,16 @@
 import { prisma } from "../../../../prisma/prisma.js"
 export class PostgresUpdateTransactionRepository {
     async execute(transactionId, updateTransactionParams) {
-        return await prisma.transaction.update({
-            where: {
-                id: transactionId,
-            },
-            data: updateTransactionParams,
-        })
+        try {
+            return await prisma.transaction.update({
+                where: {
+                    id: transactionId,
+                },
+                data: updateTransactionParams,
+            })
+        } catch (e) {
+            console.log(e)
+            return null
+        }
     }
 }
