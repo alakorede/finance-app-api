2ceaab546de0d14aafac2879627ffcb71704f0ee
Luiz Moraes <luizmoraesim@gmail.com>
2025-07-30 01:29:46 -0300

    test: ensure GetUserByIdUseCase returns user successfully

---
+++ test/use-cases/user/get-user-by-id.test.js
@@ -0,0 +1,35 @@
+import { GetUserByIdUseCase } from "../../../src/use-cases/index.js"
+import { faker } from "@faker-js/faker"
+
+describe("GetUserByIdUseCase", () => {
+    const user = {
+        id: faker.string.uuid(),
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({ length: 7 }),
+    }
+
+    class GetUserByIdRepositoryStub {
+        async execute() {
+            return user
+        }
+    }
+
+    const makeSut = () => {
+        const getUserByIdRepository = new GetUserByIdRepositoryStub()
+        const sut = new GetUserByIdUseCase(getUserByIdRepository)
+
+        return { getUserByIdRepository, sut }
+    }
+
+    test("Should return user successfully", async () => {
+        //arrange
+        const { sut } = makeSut()
+        //act
+        const result = await sut.execute(user.id)
+        //assert
+        expect(result).toBeTruthy
+        expect(result).toEqual(user)
+    })
+})
