eae1d5b99aab25d57023d17aa985a5c8785a4103
Luiz Moraes <luizmoraesim@gmail.com>
2025-08-02 13:40:53 -0300

    test: ensure PostgresCreateUserRepository sends the righ params to db

--- test/repositories/user/create-user.test.js
+++ test/repositories/user/create-user.test.js
@@ -1,15 +1,17 @@
 import { PostgresCreateUserRepository } from "../../../src/repositories/postgres/index.js"
+import { prisma } from "../../../prisma/prisma.js"
 import { faker } from "@faker-js/faker"
 
 describe("CreateUserRepository", () => {
+    const user = {
+        id: faker.string.uuid(),
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({ length: 7 }),
+    }
+
     test("Should create usar on database successfully", async () => {
-        const user = {
-            id: faker.string.uuid(),
-            first_name: faker.person.firstName(),
-            last_name: faker.person.lastName(),
-            email: faker.internet.email(),
-            password: faker.internet.password({ length: 7 }),
-        }
         const sut = new PostgresCreateUserRepository()
 
         const result = await sut.execute(user)
@@ -19,5 +21,14 @@ describe("CreateUserRepository", () => {
         expect(result.last_name).toBe(user.last_name)
         expect(result.email).toBe(user.email)
         expect(result.password).toBe(user.password)
+    })
+
+    test("Should call prisma with correct params", async () => {
+        const sut = new PostgresCreateUserRepository()
+        const prismaSpy = jest.spyOn(prisma.user, "create")
+
+        await sut.execute(user)
+
+        expect(prismaSpy).toHaveBeenCalledWith({ data: user })
     })
 })
