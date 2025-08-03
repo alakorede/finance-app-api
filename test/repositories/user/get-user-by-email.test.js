361411b9dbdf4473133257ee9845d67cfa265815
Luiz Moraes <luizmoraesim@gmail.com>
2025-08-03 12:38:58 -0300

    test: ensure PostgresGetUserByEmail returns user with a search by email from db

---
+++ test/repositories/user/get-user-by-email.test.js
@@ -0,0 +1,23 @@
+import { PostgresGetUserByEmailRepository } from "../../../src/repositories/postgres/index.js"
+import { prisma } from "../../../prisma/prisma.js"
+import { faker } from "@faker-js/faker"
+
+describe("PostgresGetUserByEmailRepository", () => {
+    test("Should get user by e-mail from db", async () => {
+        const user = await prisma.user.create({
+            data: {
+                id: faker.string.uuid(),
+                first_name: faker.person.firstName(),
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password({ length: 7 }),
+            },
+        })
+
+        const sut = new PostgresGetUserByEmailRepository()
+
+        const result = await sut.execute(user.email)
+
+        expect(result).toEqual(user)
+    })
+})
