51f264c9bc647a0789409cc2ae5b4f5583c659a7
Luiz Moraes <luizmoraesim@gmail.com>
2025-08-03 11:02:27 -0300

    test: ensure PostgresDeleteUserRepository deletes a user from db

---
+++ test/repositories/user/delete-user.test.js
@@ -0,0 +1,25 @@
+import { PostgresDeleteUserRepository } from "../../../src/repositories/postgres/index.js"
+import { prisma } from "../../../prisma/prisma.js"
+import { faker } from "@faker-js/faker"
+
+describe("PostgresDeleteUserRepository", () => {
+    const user = {
+        id: faker.string.uuid(),
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({ length: 7 }),
+    }
+
+    test("Should delete a user on db", async () => {
+        await prisma.user.create({
+            data: user,
+        })
+
+        const sut = new PostgresDeleteUserRepository()
+
+        const result = await sut.execute(user.id)
+
+        expect(result).toEqual(user)
+    })
+})
