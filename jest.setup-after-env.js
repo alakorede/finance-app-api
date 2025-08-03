5682dc92d469557600274db17216c97b15c5fffa
Luiz Moraes <luizmoraesim@gmail.com>
2025-08-03 12:28:06 -0300

    fix: added possibility to avoid user_balance_view errors on tests

--- jest.setup-after-env.js
+++ jest.setup-after-env.js
@@ -1,6 +1,9 @@
 import { prisma } from "./prisma/prisma"
 
 beforeEach(async () => {
-    await prisma.user.deleteMany({})
+    // await prisma.$queryRawUnsafe(
+    //     `TRUNCATE TABLE user_balance_view RESTART IDENTITY CASCADE;`
+    // )
     await prisma.transaction.deleteMany({})
+    await prisma.user.deleteMany({})
 })
