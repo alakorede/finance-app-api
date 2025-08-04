54ff11d80f9fc20953a17cef9d976e6e79b7ecb4
Luiz Moraes <luizmoraesim@gmail.com>
2025-08-04 01:31:55 -0300

    test: ensure GET /api/users:userId returns 200 and user Data when user is found

--- tests-e2e/routes/users.e2e.test.js
+++ tests-e2e/routes/users.e2e.test.js
@@ -3,13 +3,13 @@ import { app } from "../../index.js"
 import { faker } from "@faker-js/faker"
 
 describe("UserRoutes E2E Tests", () => {
-    test("POST /users should return 200 and user data on body when user is created", async () => {
-        const userData = {
-            first_name: faker.person.firstName(),
-            last_name: faker.person.lastName(),
-            email: faker.internet.email(),
-            password: faker.internet.password({ length: 7 }),
-        }
+    const userData = {
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({ length: 7 }),
+    }
+    test("POST /api/users should return 200 and user data on body when user is created", async () => {
         const response = await request(app).post("/api/users").send(userData)
 
         expect(response.status).toBe(200)
@@ -18,5 +18,19 @@ describe("UserRoutes E2E Tests", () => {
         expect(response.body.first_name).toBe(userData.first_name)
         expect(response.body.last_name).toBe(userData.last_name)
         expect(response.body.email).toBe(userData.email)
+    })
+
+    test("GET /api/users:userId should return 200 and user dara on body when user is found", async () => {
+        const { body: createdUser } = await request(app)
+            .post("/api/users")
+            .send(userData)
+        const response = await request(app).get(`/api/users/${createdUser.id}`)
+
+        expect(response.status).toBe(200)
+        expect(response.body.id).toBe(createdUser.id)
+        expect(response.body.password).toBe(createdUser.password)
+        expect(response.body.first_name).toBe(createdUser.first_name)
+        expect(response.body.last_name).toBe(createdUser.last_name)
+        expect(response.body.email).toBe(createdUser.email)
     })
 })
