d103df29efdd62a4c36c6045edeafcf204691108
Luiz Moraes <luizmoraesim@gmail.com>
2025-08-03 22:04:43 -0300

    fix: changed jest.global-setup.js config to ensure user_balance_view creation

--- jest.global-setup.js
+++ jest.global-setup.js
@@ -2,10 +2,7 @@ import { execSync } from "child_process"
 
 module.exports = async function () {
     //Use this in case to receive errors about user_balance_view missing
-    // execSync("docker-compose down -v") // Remove volumes, apaga o banco
-    // execSync("docker-compose up -d --wait postgres-test")
-    // execSync("npx prisma migrate deploy")
-
+    execSync("docker-compose down postgres-test") // Remove volumes, apaga o banco
     execSync("docker-compose up -d --wait postgres-test")
-    execSync("npx prisma db push")
+    execSync("npx prisma migrate deploy")
 }
