3d1e7c521dbd71300e98cf756eed308b04045c03
Luiz Moraes <luizmoraesim@gmail.com>
2025-08-03 22:24:15 -0300

    test: ensure PostgresCreateTransaction creates a transaction on db

---
+++ test/repositories/transaction/create-transaction.test.js
@@ -0,0 +1,41 @@
+import { PostgresCreateTransactionRepository } from "../../../src/repositories/postgres/index.js"
+import { prisma } from "../../../prisma/prisma.js"
+import { faker } from "@faker-js/faker"
+import dayjs from "dayjs"
+
+describe("PostgresCreateTransactionRepository", () => {
+    const user = {
+        id: faker.string.uuid(),
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({ length: 7 }),
+    }
+
+    const transaction = {
+        id: faker.string.uuid(),
+        user_id: user.id,
+        name: faker.finance.accountName(),
+        date: faker.date.anytime().toISOString(),
+        amount: Number(faker.finance.amount(10, 1000)),
+        type: faker.helpers.arrayElement(["EXPENSE", "INVESTMENT", "EARNING"]),
+    }
+
+    test("Should create a transaction on db", async () => {
+        await prisma.user.create({ data: user })
+        const sut = new PostgresCreateTransactionRepository()
+
+        const result = await sut.execute(transaction)
+
+        expect(result.id).toBe(transaction.id)
+        expect(result.user_id).toBe(transaction.user_id)
+        expect(result.name).toBe(transaction.name)
+        expect(dayjs(result.date).daysInMonth()).toBe(
+            dayjs(transaction.date).daysInMonth(),
+        )
+        expect(dayjs(result.date).month()).toBe(dayjs(transaction.date).month())
+        expect(dayjs(result.date).year()).toBe(dayjs(transaction.date).year())
+        expect(Number(result.amount)).toBe(transaction.amount)
+        expect(result.type).toBe(transaction.type)
+    })
+})
