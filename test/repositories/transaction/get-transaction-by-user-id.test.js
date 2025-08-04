ffdcf67ba5ace92727c60ddb614896f2dfb0ac51
Luiz Moraes <luizmoraesim@gmail.com>
2025-08-03 22:52:37 -0300

    test: ensure PostgresGetTransactionByUserIdRespository returns a transaction from db

---
+++ test/repositories/transaction/get-transaction-by-user-id.test.js
@@ -0,0 +1,47 @@
+import { PostgresGetTransactionsByUserIdRepository } from "../../../src/repositories/postgres/index.js"
+import { prisma } from "../../../prisma/prisma.js"
+import { faker } from "@faker-js/faker"
+import dayjs from "dayjs"
+
+describe("PostgresGetTransactionByUserId", () => {
+    const user = {
+        id: faker.string.uuid(),
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({ length: 7 }),
+    }
+
+    const transaction = {
+        id: faker.string.uuid(),
+        user_id: user.id,
+        name: faker.finance.accountName(),
+        date: faker.date.anytime().toISOString(),
+        amount: Number(faker.finance.amount(10, 1000)),
+        type: faker.helpers.arrayElement(["EXPENSE", "INVESTMENT", "EARNING"]),
+    }
+
+    test("Should return transaction taken by userID from db", async () => {
+        await prisma.user.create({ data: user })
+        await prisma.transaction.create({ data: transaction })
+
+        const sut = new PostgresGetTransactionsByUserIdRepository()
+
+        const result = await sut.execute(user.id)
+
+        expect(result[0].id).toBe(transaction.id)
+        expect(result[0].user_id).toBe(transaction.user_id)
+        expect(result[0].name).toBe(transaction.name)
+        expect(dayjs(result[0].date).daysInMonth()).toBe(
+            dayjs(transaction.date).daysInMonth(),
+        )
+        expect(dayjs(result[0].date).month()).toBe(
+            dayjs(transaction.date).month(),
+        )
+        expect(dayjs(result[0].date).year()).toBe(
+            dayjs(transaction.date).year(),
+        )
+        expect(Number(result[0].amount)).toBe(transaction.amount)
+        expect(result[0].type).toBe(transaction.type)
+    })
+})
