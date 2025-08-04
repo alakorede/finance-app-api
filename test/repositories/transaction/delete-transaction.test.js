3ffbe6061d6c1bdb3747e8197240ce4aad2e9230
Luiz Moraes <luizmoraesim@gmail.com>
2025-08-03 22:35:10 -0300

    test: ensure PostgresDeleteTransactionRepository deletes a Transaction on db

---
+++ test/repositories/transaction/delete-transaction.test.js
@@ -0,0 +1,43 @@
+import { PostgresDeleteTransactionRepository } from "../../../src/repositories/postgres/index.js"
+import { prisma } from "../../../prisma/prisma.js"
+import { faker } from "@faker-js/faker"
+import dayjs from "dayjs"
+
+describe("PostgresDeleteTransactionRepository", () => {
+    const user = {
+        id: faker.string.uuid(),
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({ length: 7 }),
+    }
+
+    const transaction = {
+        id: faker.string.uuid(),
+        user_id: user.id,
+        name: faker.finance.accountName(),
+        date: faker.date.anytime().toISOString(),
+        amount: Number(faker.finance.amount(10, 1000)),
+        type: faker.helpers.arrayElement(["EXPENSE", "INVESTMENT", "EARNING"]),
+    }
+
+    test("Should delete a transaction from db", async () => {
+        await prisma.user.create({ data: user })
+        await prisma.transaction.create({ data: transaction })
+
+        const sut = new PostgresDeleteTransactionRepository()
+
+        const result = await sut.execute(transaction.id)
+
+        expect(result.id).toBe(transaction.id)
+        expect(result.user_id).toBe(transaction.user_id)
+        expect(result.name).toBe(transaction.name)
+        expect(dayjs(result.date).daysInMonth()).toBe(
+            dayjs(transaction.date).daysInMonth(),
+        )
+        expect(dayjs(result.date).month()).toBe(dayjs(transaction.date).month())
+        expect(dayjs(result.date).year()).toBe(dayjs(transaction.date).year())
+        expect(Number(result.amount)).toBe(transaction.amount)
+        expect(result.type).toBe(transaction.type)
+    })
+})
