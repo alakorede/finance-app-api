278e59e9a15be2c87ae90f5d869480e4bcdfefe4
Luiz Moraes <luizmoraesim@gmail.com>
2025-08-06 02:37:29 -0300

    chore: finishing babel remove

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,18 @@
 /** @type {import('jest').Config} */
-const config = {
+// const config = {
+//     collectCoverageFrom: ["src/**/*.js"],
+//     coverageDirectory: "coverage",
+//     coverageProvider: "v8",
+//     globalSetup: "<rootDir>/jest.global-setup.js",
+//     setupFilesAfterEnv: ["<rootDir>/jest.setup-after-env.js"],
+// }
+
+// export default config
+export default {
+    globalSetup: "<rootDir>/jest.global-setup.js",
+    setupFilesAfterEnv: ["<rootDir>/jest.setup-after-env.js"],
     collectCoverageFrom: ["src/**/*.js"],
     coverageDirectory: "coverage",
     coverageProvider: "v8",
-    globalSetup: "<rootDir>/jest.global-setup.js",
-    setupFilesAfterEnv: ["<rootDir>/jest.setup-after-env.js"],
+    // Sem transform!
 }
-
-export default config
--- jest.global-setup.js
+++ jest.global-setup.js
@@ -1,8 +1,8 @@
 import { execSync } from "child_process"
 
-module.exports = async function () {
-    //Use this in case to receive errors about user_balance_view missing
-    //execSync("docker compose down postgres-test") // Remove volumes, apaga o banco
+export default async function () {
+    // Use this in caso receba erro de user_balance_view faltando
+    // execSync("docker compose down postgres-test") // Remove volumes, apaga o banco
     execSync("docker compose up -d --wait postgres-test")
     execSync("npx prisma migrate deploy")
 }
