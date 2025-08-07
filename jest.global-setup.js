64498bd4a0fe4f4eb511df3a7f07a7e29b7ef4dc
Luiz Moraes <luizmoraesim@gmail.com>
2025-08-07 00:00:03 -0300

    chore: add jsonwetoken to the project

--- jest.global-setup.js
+++ jest.global-setup.js
@@ -2,7 +2,7 @@ import { execSync } from "child_process"
 
 export default async function () {
     // Use this in caso receba erro de user_balance_view faltando
-    // execSync("docker compose down postgres-test") // Remove volumes, apaga o banco
+    execSync("docker compose down postgres-test") // Remove volumes, apaga o banco
     execSync("docker compose up -d --wait postgres-test")
     execSync("npx prisma migrate deploy")
 }
