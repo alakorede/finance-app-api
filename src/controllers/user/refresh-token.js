63b5139a1f277e8da2ab11217f2fee71515a1fce
Luiz Moraes <luizmoraesim@gmail.com>
2025-08-15 16:08:33 -0300

    feat: add RefreshTokenController

---
+++ src/controllers/user/refresh-token.js
@@ -0,0 +1,44 @@
+import { ZodError } from "zod"
+import {
+    unauthorizedResponse,
+    serverReturn,
+    internalServerError,
+} from "../helpers/index.js"
+
+import { refreshTokenSchema } from "../../schemas/index.js"
+import { UnauthorizedError } from "../../errors/user.js"
+
+export class RefreshTokenController {
+    constructor(refreshTokenUseCase) {
+        this.refreshTokenUseCase = refreshTokenUseCase
+    }
+
+    async execute(httpRequest) {
+        try {
+            const params = httpRequest.body
+
+            await refreshTokenSchema.parseAsync(params)
+
+            const refreshedToken = this.refreshTokenUseCase.execute(
+                params.refreshToken,
+            )
+
+            if (!refreshedToken) {
+                return unauthorizedResponse()
+            }
+
+            return serverReturn(200, refreshedToken)
+        } catch (e) {
+            if (e instanceof ZodError) {
+                return serverReturn(400, { message: e.errors[0].message })
+            }
+
+            if (e instanceof UnauthorizedError) {
+                unauthorizedResponse()
+            }
+
+            console.error(e)
+            return internalServerError()
+        }
+    }
+}
