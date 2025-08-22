56ba42ac007f88a164bf101723622cd07395674b
Luiz Moraes <luizmoraesim@gmail.com>
2025-08-22 01:02:22 -0300

    feat: ensure percentage per transaction is returned in balance route

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -52,15 +52,32 @@ export class PostgresGetUserBalanceRepository {
         const _totalEarnings = totalEarnings || new Prisma.Decimal(0)
         const _totalExpenses = totalExpenses || new Prisma.Decimal(0)
         const _totalInvestments = totalInvestments || new Prisma.Decimal(0)
+        //The method .plus() comes from Prisma.Decimal() return
+        const total = _totalEarnings
+            .plus(_totalExpenses)
+            .plus(_totalInvestments)
 
-        const balance = new Prisma.Decimal(
-            _totalEarnings - _totalExpenses - _totalInvestments,
-        )
+        const balance = _totalEarnings
+            .minus(_totalExpenses)
+            .minus(_totalInvestments)
+
+        const earningsPercentage = total.isZero()
+            ? 0
+            : _totalEarnings.div(total).times(100).floor()
+        const expensesPercentage = total.isZero()
+            ? 0
+            : _totalExpenses.div(total).times(100).floor()
+        const investmentsPercentage = total.isZero()
+            ? 0
+            : _totalInvestments.div(total).times(100).floor()
 
         return {
             earnings: _totalEarnings,
             expenses: _totalExpenses,
             investments: _totalInvestments,
+            earningsPercentage,
+            expensesPercentage,
+            investmentsPercentage,
             balance,
         }
 
