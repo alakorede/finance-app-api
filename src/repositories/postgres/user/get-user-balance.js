ff51c4772d4b170c4f141bd1d8ca0adf847e0865
Luiz Moraes <luizmoraesim@gmail.com>
2025-08-22 00:13:24 -0300

    feat: add date parameters on repository and changed the prisma query

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -1,40 +1,73 @@
 import { prisma } from "../../../../prisma/prisma.js"
+import { Prisma, TransactionType } from "@prisma/client"
 
 export class PostgresGetUserBalanceRepository {
-    async execute(userId) {
-        //Prisma n suporta view, então precisei criar uma query direto mesmo, tem usar a estrutura do prisma pra fazer a consulta
-        return await prisma.$queryRaw`
-        SELECT * FROM user_balance_view WHERE user_id = ${userId}
-      `
+    async execute(userId, from, to) {
+        const dateFilter = {
+            date: {
+                gte: new Date(from),
+                lte: new Date(to),
+            },
+        }
 
-        //Se eu não tivesse a view, usaria o o agregate pra fazer essa consulta, segue exemplo:
-        // const {_sum: {amount: totalExpenses}} = await prisma.transaction.aggregate({
-        //     where: {
-        //         user_id: userId,
-        //         type: "EXPENSE",
-        //     },
-        //     _sum: {
-        //         amount: true,
-        //     },
-        // })
-        // const {_sum: {amount: totalEarnings}} = await prisma.transaction.aggregate({
-        //     where: {
-        //         user_id: userId,
-        //         type: "EARNING",
-        //     },
-        //     _sum: {
-        //         amount: true,
-        //     },
-        // })
-        // const {_sum: {amount: totalInvestments}} = await prisma.transaction.aggregate({
-        //     where: {
-        //         user_id: userId,
-        //         type: "INVESTMENT",
-        //     },
-        //     _sum: {
-        //         amount: true,
-        //     },
-        // })
-        // const balance = totalEarnings - totalExpenses - totalInvestments
+        const {
+            _sum: { amount: totalExpenses },
+        } = await prisma.transaction.aggregate({
+            where: {
+                user_id: userId,
+                type: TransactionType.EXPENSE,
+                ...dateFilter,
+            },
+            _sum: {
+                amount: true,
+            },
+        })
+
+        const {
+            _sum: { amount: totalEarnings },
+        } = await prisma.transaction.aggregate({
+            where: {
+                user_id: userId,
+                type: TransactionType.EARNING,
+                ...dateFilter,
+            },
+            _sum: {
+                amount: true,
+            },
+        })
+
+        const {
+            _sum: { amount: totalInvestments },
+        } = await prisma.transaction.aggregate({
+            where: {
+                user_id: userId,
+                type: TransactionType.INVESTMENT,
+                ...dateFilter,
+            },
+            _sum: {
+                amount: true,
+            },
+        })
+
+        const _totalEarnings = totalEarnings || new Prisma.Decimal(0)
+        const _totalExpenses = totalExpenses || new Prisma.Decimal(0)
+        const _totalInvestments = totalInvestments || new Prisma.Decimal(0)
+
+        const balance = new Prisma.Decimal(
+            _totalEarnings - _totalExpenses - _totalInvestments,
+        )
+
+        return {
+            earnings: _totalEarnings,
+            expenses: _totalExpenses,
+            investments: _totalInvestments,
+            balance,
+        }
+
+        //Used to consume the view, but it does note have the option to filter with date parameters
+        //Prisma n suporta view, então precisei criar uma query direto mesmo, tem usar a estrutura do prisma pra fazer a consulta
+        //   return await prisma.$queryRaw`
+        //   SELECT * FROM user_balance_view WHERE user_id = ${userId}
+        // `
     }
 }
